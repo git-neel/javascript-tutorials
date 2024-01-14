9adeb1b78807b8d8bae829d9d0fe931c60880dad
git-neel <135586004+git-neel@users.noreply.github.com>
2024-01-14 05:19:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+//alert(2+2)// work only on browser and we are using node.js not browser
+
+let name = "neel" //string datatype
+let age = 34 // number datatype => 2topower53, bigint
+let loggedIn = true // boolean datatype
+let null1 = null// null is standalone value
+//undefined =>
+//symbol to find uniqueness
+
+
+//object
+
+console.log(typeof null)// type of null is object
+console.log(typeof undefined)// type of undefined is undefined
