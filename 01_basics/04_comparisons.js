c38e983dfef3d86c29b470e44ba749ed4b5e65f7
git-neel <135586004+git-neel@users.noreply.github.com>
2024-01-14 06:13:07 +0000

    basic comparison examples

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+console.log(2>"")//true
+console.log(2>"sa")//false
+console.log(2>="sa")//false
+
+console.log(""<2)//true
+
+console.log(null > 0)//false
+console.log(null == 0)//false
+console.log(null >= 0)//true
+console.log(null === 0)//false
+
+console.log("2" == 2)//true
+//console.log("2" ==== 2)// datatype different gives error
